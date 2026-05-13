7912143e7a4bb24215cf9feb6386d5b7b714df28
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2026-05-13 18:45:47 +0700

    added getMeja section (untuk nantinya di fetch)

--- fe/ujang_ngopi/src/api/api.js
+++ fe/ujang_ngopi/src/api/api.js
@@ -3,6 +3,9 @@ const API_BASE = "http://localhost:3000";
 export const fetchMenu = () =>
   fetch(`${API_BASE}/api/get_menu`).then((r) => r.json());
 
+export const getMeja = () =>
+  fetch(`${API_BASE}/api/konfigurasi/get_kursi`).then((r) => r.json());
+
 export const fetchKategori = () =>
   fetch(`${API_BASE}/api/tabel_kategori`).then((r) => r.json());
 
