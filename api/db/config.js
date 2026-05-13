af2e1dd3c195e618ed2bbd67da11cebff044e767
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2026-05-13 15:30:51 +0700

    sistem cabang

--- api/db/config.js
+++ api/db/config.js
@@ -4,12 +4,13 @@ require("dotenv").config();
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    database: process.env.DB_NAME,
+    database: process.env.CABANG_TENANT + "_" +process.env.DB_NAME,
     port: process.env.DB_PORT,
     password : process.env.DB_PASSWORD
 });
 
 db.connect((err) => {
+    console.log(process.env.CABANG_TENANT + "_" +process.env.DB_NAME)
     if (err) {
         console.error("Database connection failed:", err.message);
         return;
