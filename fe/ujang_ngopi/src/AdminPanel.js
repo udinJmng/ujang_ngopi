30d2096b681763da5a46984348c5b4eaee97eb37
UJ4NG <86789381+udinJmng@users.noreply.github.com>
2026-05-14 22:23:37 +0700

    change from base raw url into .env

--- fe/ujang_ngopi/src/AdminPanel.js
+++ fe/ujang_ngopi/src/AdminPanel.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 import "./styles/panel.css";
 
-const API_BASE = "http://localhost:3000";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:3000";
 
 const emptyForm = { fName: "", lName: "", username_login: "", password_login: "", avatar_url: "" };
 
